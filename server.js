8f1a152e0b921abb8fd8c15ba0f988cbed41eeee
塏翔 潘 <settour.frontend@gmail.com>
2017-05-18 20:40:12 +0800

    want to try heroku

--- server.js
+++ server.js
@@ -2,9 +2,22 @@ var express = require('express');
 
 //Create my app
 var app = express();
+//給heroku用的port
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req,res,next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+     next();
+  } else {
+     res.redirect('http://'+ req.hostname + req.url);
+  }
+});
+
 
 app.use(express.static('public'))//第二個參數是檔案名稱
 
-app.listen(3000,function (){
-    console.log('Express server is up on port 3000');
+
+
+app.listen(PORT,function (){
+    console.log('Express server is up on port 3000'+ PORT);
 });
