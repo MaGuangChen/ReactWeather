106e62ff99423b031bb90cf12d460f15406b7045
塏翔 潘 <settour.frontend@gmail.com>
2017-05-19 10:52:00 +0800

    stle nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -8,16 +8,52 @@ var {Link, IndexLink} = require('react-router');
 還有其他的feature
 */
 
-let Nav = (props) => {
-    return (
-            <div>
-               <h2>Nav Component</h2>
-               <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
-               <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</Link>
-               <Link to="/Examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</Link>
-            </div>
+let Nav = React.createClass({
+   onSearch: function(e){
+      e.preventDefault();
+      alert('還沒用好');
+   } ,
+   render: function(){
+     return (
+         <div className="top-bar">
+
+           <div className="top-bar-left">
+
+              <ul className="menu">
+                <li className="menu-text">React 天氣應用</li>
+                <li>
+                  <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>看今日天氣</IndexLink>
+                </li>
+                <li>
+                  <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>關於Paul</Link>
+                </li>
+                <li>
+                  <Link to="/Examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>一些範例</Link>
+                </li>
+              </ul>
+          
+           </div>
+
+           <div className="top-bar-right">
+              <form onSubmit={this.onSearch}>
+                <ul className="menu">
+                   <li>
+                      <input type="search" placeholder="輸入所在地"/>
+                   </li>
+                   <li>
+                       <input type="submit" className="button" value="取得本日氣溫" />
+                   </li>
+                </ul>
+              
+              </form>
+           </div>
+         
+         </div>
         );
-}
+       }
+});
+
+
 module.exports = Nav;
 
 /*es5 做法
@@ -33,4 +69,37 @@ var Nav = React.createClass({
         );
     }
 });
+*/
+/*
+let Nav = (props) => {
+    return (
+         <div className="top-bar">
+
+           <div className="top-bar-left">
+
+              <ul className="menu">
+                <li className="menu-text">React 天氣應用</li>
+                <li>
+                  <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>看今日天氣</IndexLink>
+                </li>
+                <li>
+                  <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>關於Paul</Link>
+                </li>
+                <li>
+                  <Link to="/Examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>一些範例</Link>
+                </li>
+              </ul>
+          
+           </div>
+
+           <div className="top-bar-right">
+              <form onSubmit={this.onSearch}>
+              
+              
+              </form>
+           </div>
+         
+         </div>
+        );
+}
 */
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,19 @@
 var webpack = require('webpack');
-
 module.exports = {
   entry: [
-      'script!jquery/dist/jquery.min.js',//先進jquery，加script的原因是因為本身不是webpack專屬的套件
-      'script!foundation-sites/dist/foundation.min.js',//再進foundation
-      './app/app.jsx'//最後進檔案
-  ] ,//入口
+    'script!jquery/dist/jquery.min.js',//先進jquery，加script的原因是因為本身不是webpack專屬的套件
+    'script!foundation-sites/dist/foundation.min.js',//再進foundation
+    './app/app.jsx'//最後進檔案
+  ],//入口
   externals: {
-     jquery: 'jQery'
+    jquery: 'jQuery'
   },
-  plugins: {
-     //provide plugins可以省略一些像是＄號或是其他的
-     new webpack.ProvidePlugin({
-         '$': 'jquery',
-         'jQuery':'jquery'
-     })
-  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
       path:__dirname,
       filename: './public/bundle.js'//生成一個bundle.js
