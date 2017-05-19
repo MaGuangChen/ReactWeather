33760a0bb031e54273c6ccc5875fba3dc7e089c2
塏翔 潘 <settour.frontend@gmail.com>
2017-05-19 11:31:39 +0800

    add foundation grid and example link

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,20 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 
 let Examples = (props) => {
+  //加入了fundation class，並且引入router然後讓兩個li能夠進入連結首頁網址加上location
  return (
            <div>
-             <h3>範例</h3>
-             <p>之後會陸續增加範例</p>
+             <h1 className="text-center">尋找搜尋想法</h1>
+             <p>這邊有一些可以測試的地點</p>
+             <ol>
+               <li>
+                  <Link to='/?location=Taipei'>`Taipei, Taiwan 台北,台灣`</Link>
+               </li>
+               <li>
+                  <Link to='/?location=Rio'>`Rio, Brazil 里約, 巴西`</Link>
+               </li>
+             </ol>
            </div>
         );
 };
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,12 +1,16 @@
 var React = require('react');
 var Nav = require('Nav');
-//因為我們的Route設定為這個為父component，所以有.children
+//因為我們的Route設定為這個為父component，所以有.children，可以影響所有在其底下的page
 let Main = (props) => {
     return (
         <div>
          <Nav/>
-         <h2>Main Component</h2>
-         {props.children}
+         <div className="row">
+            <div className="columns medium-6 large-4 small-centered">
+               {props.children}
+            </div>
+         </div>
+         
          </div>
        );
 }
